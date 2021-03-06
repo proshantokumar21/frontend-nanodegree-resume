2041bd2ee26ed11b5d007b53ac377514ddfd3233
Proshanto Kumar <23499782+proshantokumar21@users.noreply.github.com>
2021-03-06 18:42:50 +0600

    Update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,14 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-$("#main").append("Proshanto");
+
+var bio = {
+    "name" : "Proshanto Kumar",
+    "role" : "Web Developer",
+    "contact_info" : "01855712725",
+    "picture_URL" : "images/fry.jpg",
+    "wel_message" : "Hi there! How are you?",
+    "skills" : ["Skill 1","Skill 2", "Skill 3"]
+};
+
+$("#main").append(bio.name);
