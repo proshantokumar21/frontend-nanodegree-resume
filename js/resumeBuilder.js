1e4894b120619bf35be5af26499f735292a002de
Proshanto Kumar <23499782+proshantokumar21@users.noreply.github.com>
2021-03-08 17:32:14 +0600

    End Data Types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,15 +6,77 @@ This is empty on purpose! Your code to build the resume will go here.
 var bio = {
     "name" : "Proshanto Kumar",
     "role" : "Web Developer",
-    "contact_info" : {
+    "contacts" : {
         "mobile" : "01855712725",
         "email" : "proshantokumer2@gmail.com",
         "github" : "proshantokumer2",
-        "facebook" : "kumar.du.pro"
-    };
-    "picture_URL" : "images/fry.jpg",
-    "wel_message" : "Hi there! How are you?",
+        "twitter" : "kumar.du.pro",
+        "location" : "Dhaka"
+    },
+    "bioPic" : "images/fry.jpg",
+    "welcomeMessage" : "Hi there! How are you?",
     "skills" : ["Skill 1","Skill 2", "Skill 3"]
-};
+}
 
-$("#main").append(bio.name);
+var work = {
+    "jobs" : [
+        {
+            "employer" : "Tuition",
+            "title" : "Teacher",
+            "dates" : "August 2018 - Present",
+            "description" : "I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students. I teach Students."
+        },
+        {
+            "employer" : "Neobux",
+            "title" : "Ad_Clicker",
+            "dates" : "January 2017 - December 2017",
+            "description" : "I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads. I click ads."
+        }
+    ]
+}
+
+
+
+var education = {
+    "schools" : [
+        {
+            "name" : "Notre Dame College",
+            "dates" : "2015-2017",
+            "city" : "Dhaka",
+            "majors" : ["Science"],
+            "degree" : "HSC",
+            "url" : "notredamecollegedhaka"
+        },
+        {
+            "name" : "BKSP Public School",
+            "dates" : "2005-2015",
+            "city" : "Dhaka",
+            "majors" : ["Science"],
+            "degree" : "SSC",
+            "url" : "bksppublicschool"
+        }
+    ],
+
+    "onlineCourses" : [
+        {
+            "title" : "JavaScript",
+            "school" : "Udacity",
+            "dates" : 2021,
+            "url" : "Find Your PC"
+        }
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            "title" : "Sample Project 1",
+            "dates" : 2021,
+            "description" : "Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here. Give your description here.",
+            "images" : [
+                "/images/197x148.gif",
+                "/images/fry.jpg"
+            ]
+        }
+    ]
+}
