84fc73d7ce8e76a0878f0f2c6afb732a30404f14
Proshanto Kumar <23499782+proshantokumar21@users.noreply.github.com>
2021-03-08 16:40:24 +0600

    Object Literal Notation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,7 +6,12 @@ This is empty on purpose! Your code to build the resume will go here.
 var bio = {
     "name" : "Proshanto Kumar",
     "role" : "Web Developer",
-    "contact_info" : "01855712725",
+    "contact_info" : {
+        "mobile" : "01855712725",
+        "email" : "proshantokumer2@gmail.com",
+        "github" : "proshantokumer2",
+        "facebook" : "kumar.du.pro"
+    };
     "picture_URL" : "images/fry.jpg",
     "wel_message" : "Hi there! How are you?",
     "skills" : ["Skill 1","Skill 2", "Skill 3"]
